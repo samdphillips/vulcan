72f2187749392791156a846be398e2ee421e9a63
Sam Phillips <samdphillips@gmail.com>
2022-06-10 17:30:14 -0700

    Basic test suite

---
+++ test_eval.py
@@ -0,0 +1,20 @@
+
+from unittest import TestCase
+
+import ast, read
+from eval import Interpreter
+
+
+class TestInterpreter(TestCase):
+    def setUp(self):
+        from eval import Interpreter
+        self.intp = Interpreter()
+
+    def to_ast(self, s):
+        a_sexp = read.read_all(s)
+        return ast.sexp_to_ast_seq(a_sexp)
+
+    def test_basic(self):
+        an_ast = self.to_ast('(#%let ([x (#%datum 42)])\n  x)')
+        self.assertEqual(self.intp.eval(an_ast), 42)
+
