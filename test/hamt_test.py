0c2b1d82950a5c8d284415f0ef758db7902a00d5
Sam Phillips <samdphillips@gmail.com>
2023-08-19 14:12:24 -0700

    HAMT implementation

--- test/hamt_test.py
+++ test/hamt_test.py
@@ -1,7 +1,7 @@
 
 import pytest
 
-from vulcan.hamt import SVector
+from vulcan.hamt import Hamt, SVector
 
 class TestSVector:
     def test_bad_mask(self):
@@ -69,3 +69,150 @@ class TestSVector:
         sv1 = sv0.update(3, 6, [3, 4])
         assert sv1.mask == 6
         assert sv1.slots == [3, 4]
+
+class O:
+    def __init__(self, s, h):
+        self.s = s
+        self.h = h
+
+    def __hash__(self):
+        return (super().__hash__() << self.s) | self.h
+
+class TestHamt:
+    def setup(self):
+        self.h0 = Hamt()
+        self.ka = O(8, 0x00)
+        self.kb = O(8, 0x01)
+        self.kc = O(8, 0x10)
+        self.kd = O(8, 0x20)
+
+    def test_count(self):
+        assert self.h0.count() == 0
+        h1 = self.h0.insert(self.ka, 42)
+        assert h1.count() == 1
+        h2 = h1.insert(self.kb, 43)
+        assert h2.count() == 2
+        h3 = h2.remove(self.ka)
+        assert h3.count() == 1
+        h4 = h3.remove(self.kb)
+        assert h4.count() == 0
+
+    def test_insert_ref(self):
+        h1 = self.h0.insert(self.ka, 42)
+        h2 = h1.insert(self.kb, 43)
+        assert self.h0.lookup(self.ka, lambda: None, lambda k,v: v) is None
+        assert self.h0.lookup(self.kb, lambda: None, lambda k,v: v) is None
+
+        assert h1.lookup(self.ka, lambda: None, lambda k,v: v) == 42
+        assert h1.lookup(self.kb, lambda: None, lambda k,v: v) is None
+
+        assert h2.lookup(self.ka, lambda: None, lambda k,v: v) == 42
+        assert h2.lookup(self.kb, lambda: None, lambda k,v: v) == 43
+
+    def test_insert_split(self):
+        h1 = self.h0.insert(self.ka, 42)
+        h2 = h1.insert(self.kc, 43)
+        assert h2.count() == 2
+        assert h2.lookup(self.ka, lambda: None, lambda k,v: v) == 42
+        assert h2.lookup(self.kc, lambda: None, lambda k,v: v) == 43
+
+    def test_insert_dup(self):
+        h1 = self.h0.insert(self.ka, 42)
+        h2 = h1.insert(self.ka, 42)
+        assert h2.count() == 1
+        assert h2.lookup(self.ka, lambda: None, lambda k,v: v) == 42
+
+    def test_insert_replace(self):
+        h1 = self.h0.insert(self.ka, 42)
+        h2 = h1.insert(self.ka, 43)
+        assert h2.count() == 1
+        assert h2.lookup(self.ka, lambda: None, lambda k,v: v) == 43
+
+    def test_insert_traverse(self):
+        h1 = self.h0.insert(self.ka, 0)
+        h2 = h1.insert(self.kc, 1)
+        h3 = h2.insert(self.kd, 2)
+        assert h3.count() == 3
+        assert h3.lookup(self.ka, lambda: None, lambda k,v: v) == 0
+        assert h3.lookup(self.kc, lambda: None, lambda k,v: v) == 1
+        assert h3.lookup(self.kd, lambda: None, lambda k,v: v) == 2
+
+    def test_insert_split_swap(self):
+        h1 = self.h0.insert(self.kc, 1)
+        h2 = h1.insert(self.ka, 0)
+        assert h2.count() == 2
+        assert h2.lookup(self.ka, lambda: None, lambda k,v: v) == 0
+        assert h2.lookup(self.kc, lambda: None, lambda k,v: v) == 1
+
+    def test_insert_traverse_dup(self):
+        h1 = self.h0.insert(self.ka, 0)
+        h2 = h1.insert(self.kc, 1)
+        h3 = h2.insert(self.kc, 1)
+        assert h3.count() == 2
+        assert h3.lookup(self.ka, lambda: None, lambda k,v: v) == 0
+        assert h3.lookup(self.kc, lambda: None, lambda k,v: v) == 1
+
+    def test_insert_afewbitsmore(self):
+        ka = O(8, 0)
+        kb = O(8, 0)
+        h1 = self.h0.insert(ka, 0)
+        h2 = h1.insert(kb, 1)
+        assert h2.count() == 2
+        assert h2.lookup(ka, lambda: None, lambda k,v: v) == 0
+        assert h2.lookup(kb, lambda: None, lambda k,v: v) == 1
+
+    def test_remove_traverse(self):
+        h1 = self.h0.insert(self.ka, 0)
+        h2 = h1.insert(self.kc, 1)
+        h3 = h2.insert(self.kd, 2)
+        h4 = h3.remove(self.ka)
+        assert h4.count() == 2
+        assert h4.lookup(self.ka, lambda: None, lambda k,v: v) is None
+        assert h4.lookup(self.kc, lambda: None, lambda k,v: v) == 1
+        assert h4.lookup(self.kd, lambda: None, lambda k,v: v) == 2
+
+    def test_remove_all(self):
+        h1 = self.h0.insert(self.ka, 0)
+        h2 = h1.insert(self.kb, 1)
+        h3 = h2.remove(self.ka)
+        h4 = h3.remove(self.kb)
+        assert h4.count() == 0
+        assert h4.lookup(self.ka, lambda: None, lambda k,v: v) is None
+        assert h4.lookup(self.kb, lambda: None, lambda k,v: v) is None
+
+    def test_remove_missing(self):
+        h1 = self.h0.remove(self.ka)
+        assert h1.count() == 0
+
+    def test_remove_all_traverse(self):
+        h1 = self.h0.insert(self.ka, 0)
+        h2 = h1.insert(self.kc, 1)
+        h3 = h2.insert(self.kd, 2)
+        h4 = h3.remove(self.ka)
+        h5 = h4.remove(self.kd)
+        h6 = h5.remove(self.kc)
+        assert h6.count() == 0
+        assert h6.lookup(self.ka, lambda: None, lambda k,v: v) is None
+        assert h6.lookup(self.kc, lambda: None, lambda k,v: v) is None
+        assert h6.lookup(self.kd, lambda: None, lambda k,v: v) is None
+
+    def test_remove_traverse_keep(self):
+        h1 = self.h0.insert(self.ka, 0)
+        h2 = h1.insert(self.kb, 1)
+        h3 = h2.insert(self.kc, 2)
+        h4 = h3.remove(self.ka)
+        h5 = h4.remove(self.kc)
+        assert h5.count() == 1
+        assert h5.lookup(self.ka, lambda: None, lambda k,v: v) is None
+        assert h5.lookup(self.kc, lambda: None, lambda k,v: v) is None
+        assert h5.lookup(self.kb, lambda: None, lambda k,v: v) == 1
+
+    def test_remove_traverse_missing(self):
+        h1 = self.h0.insert(self.ka, 0)
+        h2 = h1.insert(self.kb, 1)
+        h3 = h2.insert(self.kc, 2)
+        h4 = h3.remove(self.kd)
+        assert h4.count() == 3
+        assert h4.lookup(self.ka, lambda: None, lambda k,v: v) == 0
+        assert h4.lookup(self.kb, lambda: None, lambda k,v: v) == 1
+        assert h4.lookup(self.kc, lambda: None, lambda k,v: v) == 2
--- vulcan/hamt.py
+++ vulcan/hamt.py
@@ -68,3 +68,163 @@ class SVector:
         else:
             return None
 
+
+_BRANCH_FACTOR   = 16
+_TRIE_MASK_WIDTH = 4
+_TRIE_MASK       = 0xF
+_COUNT_POS       = 0
+_SUBTREE_POS     = 1
+_KEY_POS         = _SUBTREE_POS + _BRANCH_FACTOR
+_VALUE_POS       = _KEY_POS + _BRANCH_FACTOR
+
+class Hamt:
+    def __init__(self, root=None):
+        if root is None:
+            root = SVector(1, [0])
+        self.root = root
+
+    def count(self):
+        return self.root[_COUNT_POS]
+
+    def _find_insert(self, key, value, key_hash, node, depth):
+        if key_hash == 0:
+            raise Exception("out of hash bits")
+
+        i = key_hash & _TRIE_MASK
+        ti = _SUBTREE_POS + i
+        if ti in node:
+            changed, child = self._find_insert(key,
+                                               value,
+                                               key_hash >> _TRIE_MASK_WIDTH,
+                                               node[ti],
+                                               depth + 1)
+            if changed:
+                m = (1 << _COUNT_POS) | (1 << ti)
+                return True, node.update(m, m, [node[_COUNT_POS] + 1, child])
+            else:
+                return False, node
+        else:
+            ki = _KEY_POS + i
+            vi = _VALUE_POS + i
+            if ki in node:
+                # replace or split
+                old_key = node[ki]
+                if old_key == key:
+                    old_value = node[vi]
+                    if old_value == value:
+                        # same key/value don't change
+                        return False, node
+                    # replace key/value
+                    m = (1 << ki) | (1 << vi)
+                    return True, node.update(m, m, [key, value])
+                # split value
+                old_key = node[ki]
+                old_value = node[vi]
+                old_hash = hash(old_key) & ((1 << 64) - 1)
+                old_hash = old_hash >> ((depth + 1) * _TRIE_MASK_WIDTH)
+                key_hash = key_hash >> _TRIE_MASK_WIDTH
+                child = self.make_node2(old_key, old_value, old_hash,
+                                        key, value, key_hash)
+                rm = (1 << _COUNT_POS) | (1 << ki) | (1 << vi)
+                am = (1 << _COUNT_POS) | (1 << ti)
+                return True, node.update(rm, am, [node[_COUNT_POS] + 1, child])
+            # insert key/value into node
+            rm = 1 << _COUNT_POS
+            am = (1 << _COUNT_POS) | (1 << ki) | (1 << vi)
+            return True, node.update(rm, am, [node[_COUNT_POS] + 1, key, value])
+
+    def make_node2(self, ak, av, ah, bk, bv, bh):
+        ai = ah & _TRIE_MASK
+        bi = bh & _TRIE_MASK
+        if bi < ai:
+            return self.make_node2(bk, bv, bh, ak, av, ah)
+        elif bi == ai:
+            child = self.make_node2(ak, av, ah >> _TRIE_MASK_WIDTH,
+                                    bk, bv, bh >> _TRIE_MASK_WIDTH)
+            ti = _SUBTREE_POS + ai
+            m = (1 << _COUNT_POS) | (1 << ti)
+            return SVector(m, [2, child])
+        aki = _KEY_POS + ai
+        bki = _KEY_POS + bi
+        avi = _VALUE_POS + ai
+        bvi = _VALUE_POS + bi
+        m = ((1 << _COUNT_POS) |
+             (1 << aki) | (1 << avi) |
+             (1 << bki) | (1 << bvi))
+        return SVector(m, [2, ak, bk, av, bv])
+
+    def insert(self, key, value):
+        key_hash = hash(key) & ((1 << 64) - 1)
+        changed, next_root = self._find_insert(key, value, key_hash, self.root, 0)
+        if changed:
+            return Hamt(next_root)
+        return self
+
+    def lookup(self, key, fk, sk):
+        key_hash = hash(key) & ((1 << 64) - 1)
+        node = self.root
+        i = key_hash & _TRIE_MASK
+        ti = _SUBTREE_POS + i
+        while ti in node:
+            node = node[ti]
+            key_hash = key_hash >> _TRIE_MASK_WIDTH
+            if key_hash == 0:
+                raise Exception("out of hash bits")
+            i = key_hash & _TRIE_MASK
+            ti = _SUBTREE_POS + i
+        ki = _KEY_POS + i
+        if ki in node:
+            found_key = node[ki]
+            if found_key == key:
+                # node has the right key
+                vi = _VALUE_POS + i
+                return sk(found_key, node[vi])
+        # node has the wrong key OR
+        # node doesn't have key
+        return fk()
+
+    def _find_remove(self, key, key_hash, node):
+        if key_hash == 0:
+            raise Exception("out of hash bits")
+
+        i = key_hash & _TRIE_MASK
+        ti = _SUBTREE_POS + i
+        if ti in node:
+            changed, child = self._find_remove(key,
+                                               key_hash >> _TRIE_MASK_WIDTH,
+                                               node[ti])
+            if changed:
+                m = (1 << _COUNT_POS) | (1 << ti)
+                if child is None:
+                    # remove this child
+                    if node[_COUNT_POS] == 1:
+                        # no subtrees and no key/values
+                        # remove entire node
+                        return True, None
+                    return True, node.update(m, 1 << _COUNT_POS, [node[_COUNT_POS] - 1])
+                # swap child
+                return True, node.update(m, m, [node[_COUNT_POS] - 1, child])
+            return False, node
+
+        ki = _KEY_POS + i
+        if ki in node:
+            if key == node[ki]:
+                if node[_COUNT_POS] == 1:
+                    # no subtrees and no key/values
+                    # remove entire node
+                    return True, None
+                # remove key/value from node
+                vi = _VALUE_POS + i
+                rm = (1 << _COUNT_POS) | (1 << ki) | (1 << vi)
+                am = (1 << _COUNT_POS)
+                return True, node.update(rm, am, [node[_COUNT_POS] - 1])
+            # no change: key doesn't match
+        # no change: no key here
+        return False, node
+
+    def remove(self, key):
+        key_hash = hash(key) & ((1 << 64) - 1)
+        changed, next_root = self._find_remove(key, key_hash, self.root)
+        if changed:
+            return Hamt(next_root)
+        return self
